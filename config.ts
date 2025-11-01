e9e7379e5162b7cba699183b6fccc09b62e93f03
cameronapak <cameronandrewpak@gmail.com>
2025-11-01 06:50:05 -0500

    Add authentication configuration to the main config file, enabling JWT support with a secret key, and set the mode to 'db' for database operations.

--- config.ts
+++ config.ts
@@ -2,11 +2,20 @@ import type { CloudflareBkndConfig } from "bknd/adapter/cloudflare";
 import { showRoutes } from "bknd/plugins";
 
 export default {
+   config: {
+      auth: {
+         enabled: true,
+         jwt: {
+            secret: "jesus-saves"
+         },
+      }
+   },
    d1: {
       session: true,
    },
    options: {
       plugins: [showRoutes()],
+      mode: "db",
    },
    adminOptions: {
       adminBasepath: "/admin",
