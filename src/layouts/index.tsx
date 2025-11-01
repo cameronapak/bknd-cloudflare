31384a9489981b365ca593a9da61b6209434447d
cameronapak <cameronandrewpak@gmail.com>
2025-11-01 06:03:44 -0500

    Add daisyui as a dependency and configure VSCode settings for Tailwind CSS. Update global CSS to include daisyui plugin and refactor layout component by removing unnecessary styles.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,22 +1,16 @@
 /** @jsxImportSource hono/jsx */
-import type { FC, CSSProperties } from 'hono/jsx'
+import type { FC } from 'hono/jsx'
 
 export const Layout: FC<{ children: any, title: string, description: string }> = ({ children, title, description }) => (
   <html lang="en" class="wa-cloak">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      {/* <script src="https://kit.webawesome.com/b48c3e538f534567.js" crossorigin="anonymous"></script> */}
       <title>{title}</title>
       <meta name='description' content={description} />
       <link href="./styles/global.css" rel="stylesheet" />
     </head>
-    <body
-      class="bg-gray-500"
-      style={{
-        padding: 'var(--wa-space-m)',
-      } as CSSProperties}
-    >
+    <body>
       {children}
     </body>
   </html>
