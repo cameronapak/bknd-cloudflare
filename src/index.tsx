72f02b512ee63ef6f18c1f9573fd5c3e48884e7e
cameronapak <cameronandrewpak@gmail.com>
2025-11-01 05:57:58 -0500

    Enhance project with Tailwind CSS integration and update build scripts. Add global CSS file, modify package.json for Tailwind and concurrently, and adjust wrangler.json for asset directory. Update index.tsx and layout component for styling improvements.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ export default serve({
 
     app.server.get("/", (c) => c.render((
       <>
-        <h1>Hello World</h1>
+        <h1 class="text-red-500 text-xs font-bold">Hello World</h1>
         <p>This is the home page</p>
       </>
     ), {
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -6,11 +6,13 @@ export const Layout: FC<{ children: any, title: string, description: string }> =
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <script src="https://kit.webawesome.com/b48c3e538f534567.js" crossorigin="anonymous"></script>
+      {/* <script src="https://kit.webawesome.com/b48c3e538f534567.js" crossorigin="anonymous"></script> */}
       <title>{title}</title>
       <meta name='description' content={description} />
+      <link href="./styles/global.css" rel="stylesheet" />
     </head>
-    <body 
+    <body
+      class="bg-gray-500"
       style={{
         padding: 'var(--wa-space-m)',
       } as CSSProperties}
