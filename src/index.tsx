cc222098a4f31969863e4d5fa34b6d9473dbef0d
cameronapak <cameronandrewpak@gmail.com>
2025-11-04 03:46:24 -0600

    Refactor todo item rendering in index.tsx to include a form element for checkbox state management. Update checkbox to handle completion status changes via a PUT request, improving real-time interaction and user experience.

--- src/index.tsx
+++ src/index.tsx
@@ -61,17 +61,25 @@ export default serve({
       return ServerSentEventGenerator.stream(async (stream) => {        
         // Add new todo to the top of the list
         stream.patchElements((
-          <li id={`todo-${todo.id}`} class="not-prose flex items-center gap-2"  >
-            <input type="checkbox" class="checkbox checkbox-primary" />
-            <span>{todo.title}</span>
-            <button
-              data-on:click={`@delete('/todos/${todo.id}')`}
-              class="btn btn-square btn-ghost"
-              type="button"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-            </button>
-          </li>
+          <form key={todo.id}>
+            <li id={`todo-${todo.id}`} class="not-prose flex items-center gap-2"  >
+              <input
+                type="checkbox"
+                name="completed"
+                data-on:change={`@put('/todos/${todo.id}', {contentType: 'form'})`}
+                class="checkbox checkbox-primary"
+                checked={!!todo.completed_datetime}
+              />
+              <span>{todo.title}</span>
+              <button
+                data-on:click={`@delete('/todos/${todo.id}')`}
+                class="btn btn-square btn-ghost"
+                type="button"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+              </button>
+            </li>
+          </form>
         ).toString(), {
           selector: "#todos-list",
           mode: "prepend",
